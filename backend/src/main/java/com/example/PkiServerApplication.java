c69adcff93f336ed760abdca317d40fc4769c14f
kuridza1 <131389372+kuridza1@users.noreply.github.com>
2025-10-17 19:36:09 +0200

    [ADD] Spring booth server

---
+++ backend/src/main/java/com/example/PkiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiServerApplication.class, args);
+	}
+
+}
